1a773b16503ef4450f6c60fc999dee39f3d26bb0
Felipe Souza <felipedb91@gmail.com>
2021-03-02 00:04:40 -0300

    chore: create docker file

--- src/app.js
+++ src/app.js
@@ -8,16 +8,15 @@ const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml');
 const NodeHog = require('nodehog');
 const config = require('./config/system-life');
-const promBundle = require("express-prom-bundle");
+const promBundle = require('express-prom-bundle');
 
-const metricsMiddleware = promBundle({ 
-        includeMethod: true, 
-        includePath: true, 
-        customLabels: 
-            { 
-                project_version: '1.0' 
-            } 
-    });
+const metricsMiddleware = promBundle({
+  includeMethod: true,
+  includePath: true,
+  customLabels: {
+    project_version: '1.0',
+  },
+});
 
 app.use(metricsMiddleware);
 app.use(config.middlewares.healthMid);
@@ -27,46 +26,51 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const serverStatus = () => {
-    return {
-        state: 'up',
-        dbState: mongoose.STATES[mongoose.connection.readyState]
-    }
+  return {
+    state: 'up',
+    dbState: mongoose.STATES[mongoose.connection.readyState],
+  };
 };
 
 app.get('/health', (res, req) => {
-    let healthResult = serverStatus();
-    if (mongoose.connection.readyState == 0) {
-        req.statusCode = 500;
-        req.send('down');
-    } else {
-        req.json(healthResult);
-    }
+  let healthResult = serverStatus();
+  if (mongoose.connection.readyState == 0) {
+    req.statusCode = 500;
+    req.send('down');
+  } else {
+    req.json(healthResult);
+  }
 });
 
-app.get('/stress/:elemento/tempostress/:tempoStress/tempofolga/:tempoFolga/ciclos/:ciclos', (req, res) => {
-
+app.get(
+  '/stress/:elemento/tempostress/:tempoStress/tempofolga/:tempoFolga/ciclos/:ciclos',
+  (req, res) => {
     const elemento = req.params.elemento;
     const tempoStress = req.params.tempoStress * 1000;
     const tempoFolga = req.params.tempoFolga * 1000;
     const ciclos = req.params.ciclos;
     new NodeHog(elemento, tempoStress, tempoFolga, ciclos).start();
-    res.send("OK");
-});
+    res.send('OK');
+  }
+);
 
 app.use('/api/produto', product);
 
-var developer_db_url = 'mongodb://mongouser:mongopwd@localhost:27017/admin';
+var developer_db_url = 'mongodb://localhost:27017/kubedev';
 var mongoUrl = process.env.MONGODB_URI || developer_db_url;
 
 mongoose.Promise = global.Promise;
 
 var connectWithRetry = function () {
-    return mongoose.connect(mongoUrl, function (err) {
-        if (err) {
-            console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
-            setTimeout(connectWithRetry, 5000);
-        }
-    });
+  return mongoose.connect(mongoUrl, function (err) {
+    if (err) {
+      console.error(
+        'Failed to connect to mongo on startup - retrying in 5 sec',
+        err
+      );
+      setTimeout(connectWithRetry, 5000);
+    }
+  });
 };
 
 connectWithRetry();
@@ -74,5 +78,5 @@ connectWithRetry();
 var port = process.env.SERVER_PORT || 8080;
 
 app.listen(port, () => {
-    console.log('Servidor rodando na porta ' + port);
+  console.log('Servidor rodando na porta ' + port);
 });
